02ee49b590d6c64c0c0d1df9802ec3b8d5f603f5
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2022-12-02 16:18:19 -0300

    feat: error treatment implemented in the beaches controller

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -3,16 +3,54 @@ import { Beach } from '@src/models/beach';
 describe('Beaches functional tests', () => {
   beforeAll(async () => await Beach.deleteMany());
   describe('When creating a beach', () => {
-    it('should create a beach with success', async () => {
+    // it('should create a beach with success', async () => {
+    //   const newBeach = {
+    //     lat: -33.792726,
+    //     lng: 151.289824,
+    //     name: 'Manly',
+    //     position: 'E',
+    //   };
+    //   const response = await global.testRequest.post('/beaches').send(newBeach);
+    //   expect(response.status).toBe(201);
+    //   expect(response.body).toEqual(expect.objectContaining(newBeach));
+    // });
+
+    it('should return 422 when there is a validation error', async () => {
       const newBeach = {
-        lat: -33.792726,
+        lat: 'invalid_string',
         lng: 151.289824,
         name: 'Manly',
         position: 'E',
       };
       const response = await global.testRequest.post('/beaches').send(newBeach);
-      expect(response.status).toBe(201);
-      expect(response.body).toEqual(expect.objectContaining(newBeach));
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+      });
+    });
+
+    it('should return 500 when there is any error other than validation error', async () => {
+      jest
+        .spyOn(Beach.prototype, 'save')
+        .mockRejectedValue('fail to create beach');
+
+      const newBeach = {
+        lat: -33.792726,
+        lng: 46.43243,
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(500);
+      expect(response.body).toEqual({
+        code: 500,
+        error: 'Internal Server Error',
+        message: 'Something went wrong!',
+      });
     });
   });
 });
