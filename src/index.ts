6382f50317973c4c0c83502f63b4661a8b54f0d3
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2022-12-02 22:54:21 -0300

    feat: server start working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-import './util/module-alias';
+import { SetupServer } from './server';
+import config from 'config';
+
+(async (): Promise<void> => {
+  const server = new SetupServer(config.get('App.port'));
+  await server.init();
+  server.start();
+})();
